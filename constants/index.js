360e42564e97a73ff079e007cb9953ea645b4e7b
titi120 <95542592+titi120@users.noreply.github.com>
2023-07-20 10:06:18 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x0addeefdd48da97cf03019737ede333a0223c494";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
